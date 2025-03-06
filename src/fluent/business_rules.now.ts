feb5bf49d0c9775a3b3d3c37459c1f0c32b4b7a4
System Administrator <admin@example.com>
2025-03-06 22:59:42 +0530

    commit

--- src/fluent/business_rules.now.ts
+++ src/fluent/business_rules.now.ts
@@ -1,20 +1,6 @@
 import '@servicenow/sdk/global'
 import { BusinessRule } from '@servicenow/sdk/core'
-BusinessRule({
-    $id: Now.ID['br0'],
-    when: 'display',
-    script: script`
-             (function executeRule(current, previous /*null when async*/) {
-                          
-                          	// Add your code here
-                          	current.u_applies_to_table=current.u_rule.u_applies_to_table;
-                          
-                          })(current, previous);`,
-    table: 'x_75746_ntt_ebond_u_ecase_field_attribute_map',
-    name: 'Populate Apply to Table field',
-    order: 100,
-    abort_action: false,
-})
+
 BusinessRule({
     $id: Now.ID['br1'],
     when: 'after',
