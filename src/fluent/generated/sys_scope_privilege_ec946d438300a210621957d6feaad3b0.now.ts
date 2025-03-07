f8fd2552ba488a4d2c22289ae9a9876760984135
Upender Kumar <upender.kumar1@gmail.com>
2025-03-07 17:25:01 +0530

    add scope for GlideMatch

---
+++ src/fluent/generated/sys_scope_privilege_ec946d438300a210621957d6feaad3b0.now.ts
@@ -0,0 +1,10 @@
+import { CrossScopePrivilege } from '@servicenow/sdk/core'
+
+export default CrossScopePrivilege({
+    $id: 'ec946d438300a210621957d6feaad3b0',
+    operation: 'execute',
+    status: 'allowed',
+    target_name: 'GlideFilter.match',
+    target_scope: 'global',
+    target_type: 'scriptable',
+})
